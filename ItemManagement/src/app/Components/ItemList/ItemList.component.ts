ae35a94f26293a5a11bcda452f0f51ba1be3e0aa
Fernando Orozco Franco <fernando.orfraus@gmail.com>
2025-03-29 00:01:44 -0500

    Adding form to add items to the list with no format

--- ItemManagement/src/app/Components/ItemList/ItemList.component.ts
+++ ItemManagement/src/app/Components/ItemList/ItemList.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ItemList',
   templateUrl: './ItemList.component.html',
   styleUrls: ['./ItemList.component.css'],
+  imports: [ReactiveFormsModule, NgFor, NgIf]
 })
-export class ItemListComponent implements OnInit {
-  value: string = '';
+export class ItemListComponent {
+  items: string[] = [];
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
-    this.value = 'This is the list';
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      itemControl: ['', [this.validItemCheck(this.items)]]
+    });
   }
 
+  validItemCheck(currentItems: string[]): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (typeof control.value === 'string' && control.value.trim() === '') {
+        return { notEmptyString: true };
+      }
+      if (currentItems.includes(control.value)) {
+        return { valueExists: true };
+      }
+      return null;
+    };
+  }
+
+  addToList() {
+    this.form.get('itemControl')?.updateValueAndValidity();
+    if(this.form.get('itemControl')?.valid) {
+      this.items.push(this.form.controls["itemControl"].value);
+      this.form.get('itemControl')?.setValue('');
+    }
+  }
 }
