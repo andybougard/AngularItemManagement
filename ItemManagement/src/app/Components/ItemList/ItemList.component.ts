20d9b5c45a35e1dccc569cff6337f960794736d7
Fernando Orozco Franco <fernando.orfraus@gmail.com>
2025-04-01 17:24:31 -0500

    Adding Angular Material to the form

--- ItemManagement/src/app/Components/ItemList/ItemList.component.ts
+++ ItemManagement/src/app/Components/ItemList/ItemList.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
-import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { 
+  AbstractControl, 
+  FormBuilder, 
+  FormGroup, 
+  ReactiveFormsModule, 
+  ValidationErrors, 
+  ValidatorFn } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-ItemList',
   templateUrl: './ItemList.component.html',
   styleUrls: ['./ItemList.component.css'],
-  imports: [ReactiveFormsModule, NgFor, NgIf]
+  imports: [ReactiveFormsModule, NgFor, NgIf, MatInputModule, MatFormFieldModule, MatButtonModule, MatListModule, MatCardModule]
 })
 export class ItemListComponent {
   items: string[] = [];
